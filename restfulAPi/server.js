b9d7327e1f4dbbf3635a094c2115b6bd35af6a53
niteshsingh29 <niteshsingh29072000a@gmail.com>
2022-04-02 11:47:31 +0530

    server connected with port 8000

--- restfulAPi/server.js
+++ restfulAPi/server.js
@@ -1,4 +1,13 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res, next) => {
+  res.send("sending from backend...");
+});
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
